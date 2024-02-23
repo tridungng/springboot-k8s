97aae0ce60391cccb9b08084d6b6e9c33704cad1
bbyoda <h2trungvuong@gmail.com>
2024-02-23 12:57:57 +0700

    feat: impl mysql config and sql script

---
+++ src/main/resources/db/mysql/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO springboot_k8s.studentEntities(date_of_birth, full_name, gender, home_town) VALUES ('16/12/1998', 'Max Mustermann', 'MALE', 'Bonn');
+INSERT INTO springboot_k8s.studentEntities(date_of_birth, full_name, gender, home_town) VALUES ('11/10/1999', 'Maria Musterfrau', 'FEMALE', 'Bonn');
+INSERT INTO springboot_k8s.studentEntities(date_of_birth, full_name, gender, home_town) VALUES ('16/12/1998', 'Thomas Mann', 'DIVERSE', 'Berlin');
+INSERT INTO springboot_k8s.studentEntities(date_of_birth, full_name, gender, home_town) VALUES ('16/12/1998', 'Oliver Schmidt', 'MALE', 'Oberhausen');
