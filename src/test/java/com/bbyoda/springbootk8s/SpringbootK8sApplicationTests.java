09683f6277e14caf18b8cbb4b8c54ce385bce4ed
bbyoda <h2trungvuong@gmail.com>
2024-02-23 11:47:20 +0700

    feat: init project

---
+++ src/test/java/com/bbyoda/springbootk8s/SpringbootK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbyoda.springbootk8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootK8sApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
